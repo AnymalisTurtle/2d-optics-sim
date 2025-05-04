026279d2693f2d2cc40bcbd0f84cf2ca5da87f04
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2025-05-04 13:41:41 +0200

    moved vector class description to seperate file

--- vector_test.cpp
+++ vector_test.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
+#include "basic_objects\vector.cpp"
 using namespace std;
-
-class vector{
-    public:
-    int x;
-    int y;
-    int z;
-
-
-    vector(int x_init, int y_init, int z_init){
-        this->x = x_init;
-        this->y = y_init;
-        this->z = z_init;
-    }
-    
-};
 
 
 int main(int argc, char *argv[]){
+    // parsing commandline arguments
     cout << "Entered " << argc
     << " argument(s):" << endl;
 
     for (int i = 0; i < argc; i++){
         cout << i+1 << ": " << argv[i] << endl;
     }
-    cout << "\n\n\n";
+    cout << "\n\n";
+
+    // constructing an example vector
+    vector vec(1,2,3);
+    cout << "vector coord: " << "("<< vec.x <<", "<< vec.y <<", "<< vec.z <<")"<<endl;
 
     return 0;
 }
