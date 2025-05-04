d2466599ccd4f73548d94a1f1b2b124913d34d12
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2025-05-04 13:41:41 +0200

    moved vector class description to seperate file

---
+++ basic_objects/vector.cpp
@@ -0,0 +1,14 @@
+class vector{
+    public:
+    int x;
+    int y;
+    int z;
+
+
+    vector(int x_init, int y_init, int z_init){
+        this->x = x_init;
+        this->y = y_init;
+        this->z = z_init;
+    }
+    
+};
