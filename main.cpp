8d5be16ea54a5ac3e10defde721d3b2e5ec4875d
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2025-05-04 16:50:50 +0200

    lines!

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!", sf::Style::Default, sf::State::Windowed, settings);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    std::array line = 
+    {
+        sf::Vertex{sf::Vector2f(10.f, 10.f)},
+        sf::Vertex{sf::Vector2f(100.f, 100.f)} 
+    };
+
+    line[0].color = sf::Color::Red;
+    line[1].color = sf::Color::Cyan;
 
     while (window.isOpen())
     {
@@ -16,6 +28,7 @@ int main()
 
         window.clear();
         window.draw(shape);
+        window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
         window.display();
     }
 }
