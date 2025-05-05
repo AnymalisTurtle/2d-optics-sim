d5497147cf9042d278008702e25731572731aac3
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2025-05-05 20:11:24 +0200

    reflection

--- basic_objects/Ray.cpp
+++ basic_objects/Ray.cpp
@@ -17,7 +17,7 @@ class Ray{
     sf::Color c2;
 
     public:
-    Ray(Vector origin, Vector direction, int recursion_depth = 0, Ray *parent = 0){
+    Ray(Vector origin, Vector direction, int recursion_depth = 0, Ray *parent = 0, sf::Color col1 = sf::Color(256, 256, 256), sf::Color col2 = sf::Color(236, 226, 226)){
         this->u = origin;
         this->v = direction;
         this->recursion_depth = recursion_depth;
@@ -95,7 +95,7 @@ class Ray{
                         Vector outsidence = v.Vector_angle_length((this->v * -1).get_angle_rad() + 2*incidence, 1);
                         // std::cout << "with incidence " << incidence/PI << "PI and vector (" << outsidence.x << ", " << outsidence.y << ") creating new ray." << std::endl;
                         this->child = (Ray*)malloc(sizeof(Ray));
-                        *child = Ray(this->end, outsidence, (this->recursion_depth)+1, this);
+                        *child = Ray(this->end, outsidence, (this->recursion_depth)+1, this, c2, sf::Color(c2.r-30, c2.g-40, c2.b-40));
                         child->trace(pass_on_col_obj);
                     }else if (hit->get_type() == "refract"){
 
@@ -111,8 +111,8 @@ class Ray{
             child->draw(window);
         }
         std::array line = line_from_vec(u, end);
-        line[0].color = sf::Color::White;
-        line[1].color = sf::Color::White;
+        line[0].color = c1;
+        line[1].color = c2;
         window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
 
     };
