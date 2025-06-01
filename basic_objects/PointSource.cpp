314dc6dcf4d267ee70d8ed45cd55f9d96d740507
AnymalisTurtle <54914939+AnymalisTurtle@users.noreply.github.com>
2025-06-01 17:54:12 +0200

    added variables to ps

--- basic_objects/PointSource.cpp
+++ basic_objects/PointSource.cpp
@@ -7,22 +7,28 @@
 class PointSource: public Emitter{
     private:
     Vector origin;
+    double angle;
     int ray_count;
     double ray_length;
     Ray * rays = 0;
+    sf::Color color;
 
     public:
     PointSource(Vector origin, int number_of_rays, Interactable * obj, double angle = 0, double length_of_rays = 500){
         this->origin = origin;
+        this->angle = angle;
         this->ray_count = number_of_rays;
         this->ray_length = length_of_rays;
+        this->color = sf::Color::White;
         rays = new Ray [ray_count];
 
-        for (int i=0; i<ray_count; i++){
-            Vector v = origin.Vector_angle_length(PI*2*i/ray_count + angle, 1);
-            Ray r(origin, v);
+        
+
+        for (int i=0; i<this->ray_count; i++){
+            Vector v = this->origin.Vector_angle_length(PI*2*i/this->ray_count + this->angle, 1);
+            Ray r(this->origin, v, 0, 0, this->color, this->color);
             r.trace(obj);
-            rays[i] = r;
+            this->rays[i] = r;
             // std::cout<<"added ray to array"<<std::endl;
         }
     };
